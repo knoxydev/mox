250b8ca3256065febe94b8a71722bdb5f3528cba
Kamronbek <60217990+nkr413@users.noreply.github.com>
2023-04-10 23:31:14 +0500

    add: handler of commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-	println!("Hello, world!");
+#![allow(warnings)]
+
+// PACKAGES
+use std::io;
+
+// MODULES
+
+
+
+fn main()
+{
+	let args: Vec<_> = std::env::args().collect();
+
+	if args.len() == 1 {
+		println!("You should enter arguments to use the program.\n");
+		io::stdin().read_line(&mut String::new()).unwrap();
+		return;
+	}
+	
+
+	let first_arg = std::env::args().nth(1).expect("no pattern given");
+
+	match first_arg.as_ref()
+	{
+		"init" => {
+		  let db_name = std::env::args().nth(2).expect("error database name");
+		  init_md::start(db_name.to_string());
+		},
+		"add" => add_md::start(),
+		"delete" => {},
+		"select" => {},
+		"print" => {},
+		"log" => {},
+		"info" => {},
+		"cmd" => {},
+		"exit" => return,
+		_ => {
+			println!("Incorrect command !");
+			return;
+		},
+	}
 }
