19a76e691567c9b21aa2198604938e24eee1f2f8
Kamronbek <60217990+nkr413@users.noreply.github.com>
2023-04-17 02:57:24 +0500

    new: initialization main folder

--- src/init.rs
+++ src/init.rs
@@ -1,7 +1,38 @@
 pub mod init_md
 {
-	pub fn start()
+	use std::fs;
+	use std::env;
+
+
+	fn add_info(db_name: String)
 	{
-		println!("Initialized");
+		let time_date: [String; 2] = crate::time::time_fn::start();
+
+		let cwd = env::current_dir().unwrap();
+		let folder: String = String::from(cwd.to_string_lossy());
+		let os_name = String::from(env::consts::OS);
+		let created_date = &time_date[0];
+		let created_time = &time_date[1];
+
+		let info = format!("name: {folder}\nos_name: {os_name}\ncreated_date: {created_date}\ncreated_time: {created_time}");
+		fs::write("mox/init.txt", info).expect("Unable to write file");
+	}
+
+
+	pub fn start(db_name: String)
+	{
+		let DB_NAME_STR: String = format!("mox/{}.txt", &db_name);
+
+    match fs::create_dir("mox")
+    {
+			Err(why) => println!("{:?} !", why.kind()),
+			Ok(_) => {
+				fs::File::create(DB_NAME_STR);
+				fs::File::create("mox/init.txt");
+				add_info(db_name);
+			},
+		}
+
+		println!("Initialized !");
 	}
 }
